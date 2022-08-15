5648fb8a8af9c5f2e7621ef44ee2b29afbceeeba
mohamuhsin <muhsinohoho@outlook.com>
2022-08-15 12:34:17 +0300

    Todo App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import Todo from "./Components/Todo";
+
+function App() {
+  return (
+    <div>
+      <h1>My Todos</h1>
+      <Todo text="Learn React" />
+      <Todo text="Master React" />
+      <Todo text="Explore the full React course" />
+     
+    </div>
+  );
+}
+
+export default App;
